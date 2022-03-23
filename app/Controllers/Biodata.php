e602f7df936a7217fc9a0ef079dd2b30180730d4
Olifvilian <olifvilian2000@gmail.com>
2022-03-24 01:39:49 +0700

    sorce code

---
+++ app/Controllers/Biodata.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+class biodata extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Olif Vilian
+            Saya tinggal di daerah Jakarta barat
+            Hobi saya adalah scroll tiktok
+            Saya anak dari satu bersaudara";
+
+
+    }
+}
